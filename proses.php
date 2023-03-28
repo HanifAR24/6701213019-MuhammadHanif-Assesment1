564d9c635a94faf534c75d3f3882721aac6f321e
Muhammad Hanif Aulia Rasyid <muhammadhanifrasyid@gmail.com>
2023-03-28 11:33:26 +0700

    membuat operasi aritmatika menggunakan switch case

--- proses.php
+++ proses.php
@@ -1,26 +1,35 @@
 <?php
-	$angka1 = $_POST['angka1'];
-	$angka2 = $_POST['angka2'];
-	$operator = $_POST['operator'];
-
-	if ($operator == '+') {
-		$hasil = $angka1 + $angka2;
-		echo "Hasil Penambahan: " . $hasil;
-	} elseif ($operator == '-') {
-		$hasil = $angka1 - $angka2;
-		echo "Hasil Pengurangan: " . $hasil;
-	} elseif ($operator == '*') {
-		$hasil = $angka1 * $angka2;
-		echo "Hasil Perkalian: " . $hasil;
-	} elseif ($operator == '/') {
-		if ($angka2 == 0) {
-			echo "Tidak bisa melakukan pembagian dengan nol.";
-		} else {
-			$hasil = $angka1 / $angka2;
-			echo "Hasil Pembagian: " . $hasil;
-		}
-	}
+    $angka1 = $_POST['angka1'];
+    $angka2 = $_POST['angka2'];
+    $operator = $_POST['operator'];
+    
+    switch ($operator) {
+        case '+':
+            $hasil = $angka1 + $angka2;
+            echo "Hasil Penambahan: " . $hasil;
+            break;
+        case '-':
+            $hasil = $angka1 - $angka2;
+            echo "Hasil Pengurangan: " . $hasil;
+            break;
+        case '*':
+            $hasil = $angka1 * $angka2;
+            echo "Hasil Perkalian: " . $hasil;
+            break;
+        case '/':
+            if ($angka2 == 0) {
+                echo "Tidak bisa melakukan pembagian dengan nol.";
+            } else {
+                $hasil = $angka1 / $angka2;
+                echo "Hasil Pembagian: " . $hasil;
+            }
+            break;
+        default:
+            echo "Operator aritmatika tidak valid.";
+            break;
+    }
 ?>
+
 
 
 
